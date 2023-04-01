89392382afbe81ad1f9e9ddf6baf07e2199e414c
feriani96 <ferianiyasmine3@gmail.com>
2023-04-02 01:34:49 +0200

    adding invoice front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListComponent } from './invoice/list/list.component';
-import { CreateComponent } from './product/create/create.component';
-import { HomeComponent } from './product/home/home.component';
+import { CreateComponent } from './invoice/create/create.component';
 
 
 const routes: Routes = [
-  {path:'',component:ListComponent},
-
-
+  {path:'',component:CreateComponent},
 ];
 
 @NgModule({
--- src/app/invoice/create/create.component.ts
+++ src/app/invoice/create/create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -9,21 +9,44 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class CreateComponent  {
 
   TitlePage="create new invoice";
+  invSales!:FormArray<any>;
 
   constructor(private builder:FormBuilder){}
 
   invoiceForm = this.builder.group({
 
     code:this.builder.control('',Validators.required),
-    total:this.builder.control({value: 'Nancy', disabled: true}),
-  
+    total:this.builder.control({value: '0', disabled: true}),
+    sales:this.builder.array([])
+   
 
   })
-
- 
-
   addInvoice(){
-    console.log(this.invoiceForm.value)
+    console.log(this.invoiceForm.value);
+
+  }
+
+
+  addSale(){
+   this.invSales= this.invoiceForm.get('sales') as FormArray;
+   this.invSales.push(this.generateRow());
+  }
+
+
+  
+  get invsales(){
+    return this.invoiceForm.get('sales') as FormArray;
+  }
+
+
+
+  generateRow(){
+    return this.builder.group({
+      Product:this.builder.control(''),
+      Price:this.builder.control(0),
+      Qty:this.builder.control(1),
+      
+    })
   }
 
 }
