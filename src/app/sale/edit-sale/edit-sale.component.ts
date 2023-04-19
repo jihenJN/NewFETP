5697c094e1f9ccb416fc255b1c5136202547b96d
jihenJN <jihenjihen23@gmail.com>
2023-04-19 08:12:09 +0100

    edit sale en cour

--- src/app/sale/edit-sale/edit-sale.component.ts
+++ src/app/sale/edit-sale/edit-sale.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Invoice } from 'src/app/models/Invoice';
+import { Product } from 'src/app/models/Product';
+import { Sale } from 'src/app/models/Sale';
+import { SaleService } from 'src/app/services/sale.service';
 
 @Component({
   selector: 'app-edit-sale',
   templateUrl: './edit-sale.component.html',
   styleUrls: ['./edit-sale.component.css']
 })
-export class EditSaleComponent {
+export class EditSaleComponent  implements OnInit{
+  
+  saleForm: Sale = {
+    id: '',
+    quantity:0,
+    price:0,
+    tax:0,
+    discount:0,
+    available:true,
+    product:new Product,
+    invoice:new Invoice
+    
+  };
+
+
+  constructor(private route: ActivatedRoute,
+    private router:Router,
+    private saleService: SaleService) {
+      
+     }
+
+  
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((param) => {
+      var id = String(param.get('id'));
+      this.getById(id);
+    });
+  }
+
+
+
+  getById(id: string) {
+    this.saleService.getById(id).subscribe((data) => {
+      this.saleForm = data;
+    });
+  }
+
+
+
+  update() {
+    this.saleService.update(this.saleForm)
+    .subscribe({
+      next:(data) => {
+        this.router.navigate(["/listSale"]);
+      },
+      error:(err) => {
+        console.log(err);
+      }
+    })
+  }
+
 
 }
