c850dccd04bc7757abb312f0f10ec5929df73272
jihenJN <jihenjihen23@gmail.com>
2023-09-21 14:14:56 +0100

    print invoice is ok

--- src/app/invoice/layout-invoice/layout-invoice.component.ts
+++ src/app/invoice/layout-invoice/layout-invoice.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -10,49 +9,32 @@ import { ProductService } from 'src/app/services/product.service';
 import { status } from 'src/app/models/Status';
 import { ZonedDateTime, ZoneId } from '@js-joda/core';
 import { ZonedDateTimeInterceptor } from 'src/app/_helpers/ZonedDateTime.interceptor';
-
+import { Sale } from 'src/app/models/Sale';
 
 @Component({
   selector: 'app-layout-invoice',
   templateUrl: './layout-invoice.component.html',
-  styleUrls: ['./layout-invoice.component.css']
+  styleUrls: ['./layout-invoice.component.css'],
 })
-
-
-export class LayoutInvoiceComponent   implements OnInit{
-  
-  invoiceForm!:FormGroup;
+export class LayoutInvoiceComponent implements OnInit {
+  invoiceForm!: FormGroup;
   clientForm!: FormGroup;
   invInvoice: any;
+  sales?: Sale[];
 
-  invClients: any;
-  
-  invStatus: status[] = [
-    status.DRAFT,
-    status.SENT,
-    status.RECEIVED,
-    status.APPROVED,
-    status.PARTIALLY_PAID,
-    status.PAID,
-    status.OVERDUE,
-    status.VOID
-  ];
-
-
-
- invoice: Invoice = {
+  invoice: Invoice = {
     id: '',
-    code:'',
-    remarks:'',
-    tax:0,
-    discount:0,
-    date :new Date,
-    total:0,
-    status:'',
-    client:new Client,
-       
+    code: '',
+    remarks: '',
+    tax: 0,
+    discount: 0,
+    date: new Date(),
+    total: 0,
+    status: '',
+    client: new Client(),
+    sales: [],
   };
-// -----------progress bar -------------
+  // -----------progress bar -------------
 
   firstFormGroup = this._formBuilder.group({
     firstCtrl: ['', Validators.required],
@@ -61,130 +43,85 @@ export class LayoutInvoiceComponent   implements OnInit{
     secondCtrl: ['', Validators.required],
   });
 
- // ----------- end progress bar -------------
+  // ----------- end progress bar -------------
 
-  constructor(private route: ActivatedRoute,
-    private router:Router,
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
     private builder: FormBuilder,
-    private clientService: ClientService,
     private invoiceService: InvoiceService,
-    private _formBuilder: FormBuilder) {
+    private _formBuilder: FormBuilder
+  ) {
+    const zoneId = ZoneId.of(ZonedDateTimeInterceptor.UTC_ZONE_ID);
 
-      const zoneId = ZoneId.of(ZonedDateTimeInterceptor.UTC_ZONE_ID);
+    this.invoiceForm = this.builder.group({
+      code: this.builder.control('', Validators.required),
+      client: this.builder.group({
+        id: this.builder.control('', Validators.required),
+      }),
 
-   
-      this.invoiceForm = this.builder.group({
+      remarks: this.builder.control(''),
 
-        code: this.builder.control('', Validators.required),
-        client: this.builder.group({
-          id: this.builder.control('', Validators.required)
-        }),
-  
-        remarks: this.builder.control(''),
-  
-        date: [
-          ZonedDateTime.now(zoneId)
-            .format(ZonedDateTimeInterceptor.DATE_TIME_FORMAT),
-          Validators.required
-        ],
-  
-        //date: this.builder.control(Date, Validators.required),
-        discount: this.builder.control(0),
-        tax: this.builder.control(19),
-        total: this.builder.control(0),
-        status: this.builder.control('DRAFT'),
-        sales: this.builder.array([])
-  
-      })
-      
-     }
+      date: [
+        ZonedDateTime.now(zoneId).format(
+          ZonedDateTimeInterceptor.DATE_TIME_FORMAT
+        ),
+        Validators.required,
+      ],
 
+      discount: this.builder.control(0),
+      tax: this.builder.control(19),
+      total: this.builder.control(0),
+      status: this.builder.control('DRAFT'),
+      sales: this.builder.array([]),
+    });
+  }
 
   ngOnInit(): void {
-    
-    this.getClients();
-
-    
     this.route.paramMap.subscribe((param) => {
       var id = String(param.get('id'));
       this.getById(id);
     });
-   
   }
-
-
 
   getById(id: string) {
     const date = this.invoiceForm.get('date')?.value;
     console.log(date);
 
-    const formattedDate = ZonedDateTime.parse(date).format(ZonedDateTimeInterceptor.DATE_TIME_FORMAT);
+    const formattedDate = ZonedDateTime.parse(date).format(
+      ZonedDateTimeInterceptor.DATE_TIME_FORMAT
+    );
     const invoiceData = { ...this.invoiceForm.value, date: formattedDate };
 
     this.invoiceService.getById(id).subscribe((data) => {
       this.invoice = data;
-      console.log( "this.sale" , this.invoice);
-      
-      //----JN It is very important to convert product(object) to form group --------//
-            this.invoiceForm.patchValue({
-            id: this.invoice.id,
-            code: this.invoice.code,
-            remarks: this.invoice.remarks,
-            tax: this.invoice.tax,
-            discount: this.invoice.discount,
-            date: this.invoice.date,
-            total:this.invoice.total,
-            status: this.invoice.status,
-            client: this.invoice.client?.name,
-          
+      console.log('this.sale', this.invoice);
 
+      this.sales = this.invoice.sales;
 
-          });
-    
-        });
-
+      this.invoiceForm.patchValue({
+        id: this.invoice.id,
+        code: this.invoice.code,
+        remarks: this.invoice.remarks,
+        tax: this.invoice.tax,
+        discount: this.invoice.discount,
+        date: this.invoice.date,
+        total: this.invoice.total,
+        status: this.invoice.status,
+        client: this.invoice.client,
+        sales: this.sales,
+      });
+    });
   }
 
   update() {
-
-    
-    this.invoiceService.update(this.invoiceForm.value)
-    .subscribe({
-      next:(data) => {
-        this.router.navigate(["/listInvoice"]);
+    this.invoiceService.update(this.invoiceForm.value).subscribe({
+      next: (data) => {
+        this.router.navigate(['/listInvoice']);
       },
-      error:(err) => {
+      error: (err) => {
         console.log(err);
-      }
-    })
-  }
-
-  getClients() {
-    this.clientService.get().subscribe(res => {
-      this.invClients = res;
-      console.log(this.invClients);
-    })
-  }
-
-
-  clientChange() {
-
-
-    let id = this.invoiceForm.get('client.id')?.value
-
-    console.log(this.invoiceForm.get('client'));
-    console.log("id" + id);
-
-    this.clientService.getById(id).subscribe(res => {
-      console.log("id" + id);
-
-      let data: any;
-      data = res;
-      if (data != null) {
-        this.invoiceForm.get('remarks')?.setValue(data.address)
-      }
-    })
-
-
+      },
+    });
   }
 }
--- src/app/models/Invoice.ts
+++ src/app/models/Invoice.ts
@@ -1,48 +1,40 @@
-import { Client } from "./Client";
-import { Sale } from "./Sale";
+import { Client } from './Client';
+import { Sale } from './Sale';
 
 export class Invoice {
-  id:string;
+  id: string;
   code: string;
-  remarks:string;
+  remarks: string;
   discount: number;
   tax: number;
   date: Date;
   total: number;
-  status:string;
-  client?: Pick<Client,'name'> | null;
-  sales?:Sale[]
-
-
+  status: string;
+  client?: Pick<Client, 'name' | 'phone'> | null;
+  sales?: Sale[];
 
   constructor() {
-    this.id ='';
+    this.id = '';
     this.code = '';
     this.remarks = '';
     this.discount = 0;
     this.tax = 0;
-    this.date = new Date;
+    this.date = new Date();
     this.total = 0;
     this.status = 'DRAFT';
-    this.client= null;
-   
-   
-
+    this.client = null;
   }
 }
 
 export interface InvoiceDto {
-  id:string;
+  id: string;
   code: string;
-  remarks:string;
+  remarks: string;
   discount: number;
   tax: number;
   date: Date;
   total: number;
-  status:string;
-  client?: Pick<Client,'name'> | null;
-  sales?:Sale[]
+  status: string;
+  client?: Pick<Client, 'name' | 'phone'> | null;
+  sales?: Sale[];
 }
-
-
-
